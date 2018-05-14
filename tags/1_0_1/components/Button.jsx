97833a291787edadcba9a401e38740b7a79bfee1
matrynczak <matrynczak@gmail.com>
2018-05-14 19:11:31 +0200

    trunk commit

--- tags/1_0_1/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-const Button = (props) => ( 
-	<button className="btn btn-default"  {...props}>
-		{props.icon? <span className={ "glyphicon glyphicon-" + props.icon }></span> : null}
-		{' '}
-		{props.label}
-	</button>
-)
